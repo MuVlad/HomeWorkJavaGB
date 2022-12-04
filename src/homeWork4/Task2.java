4b94703f280a9d596f52b58b19915ad2608b85af
Vlad Muslimov <muslimov.vlad@mail.ru>
2022-12-04 10:44:52 +0500

    Added task2 in homeWork4

---
+++ src/homeWork4/Task2.java
@@ -0,0 +1,36 @@
+package homeWork4;
+
+import java.util.LinkedList;
+
+/**
+ * Реализуйте очередь с помощью LinkedList со следующими методами:
+ * enqueue() - помещает элемент в конец очереди,
+ * dequeue() - возвращает первый элемент из очереди и удаляет его,
+ * first() - возвращает первый элемент из очереди, не удаляя.
+ */
+public class Task2 {
+    static LinkedList<Integer> queue = new LinkedList<Integer>();
+
+    public static void main(String[] args) {
+        enqueue(1);
+        enqueue(2);
+        enqueue(3);
+        enqueue(4);
+        System.out.println(queue);
+        System.out.println(dequeue() + " -> " + queue);
+        System.out.println(first() + " -> " + queue);
+    }
+
+    private static void enqueue(int num) {
+        queue.addLast(num);
+    }
+
+    private static int dequeue() {
+        return queue.removeFirst();
+    }
+
+    private static int first() {
+        return queue.getFirst();
+    }
+}
+
